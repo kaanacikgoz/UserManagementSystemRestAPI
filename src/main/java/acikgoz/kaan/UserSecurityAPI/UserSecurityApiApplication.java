5199748dcb89a3bc50cf9486552cf27308663fb0
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-08-15 13:25:15 +0300

    Created Project

---
+++ src/main/java/acikgoz/kaan/UserSecurityAPI/UserSecurityApiApplication.java
@@ -0,0 +1,13 @@
+package acikgoz.kaan.UserSecurityAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSecurityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSecurityApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/acikgoz/kaan/UserSecurityAPI/UserSecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package acikgoz.kaan.UserSecurityAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSecurityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
