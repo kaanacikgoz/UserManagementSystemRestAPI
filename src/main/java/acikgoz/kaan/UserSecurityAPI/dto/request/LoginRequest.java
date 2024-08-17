be76281c671c79199506b7ddf26f855c5c478445
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-08-17 04:09:27 +0300

    Added dto requests for register and login

--- src/main/java/acikgoz/kaan/UserSecurityAPI/dto/request/LoginRequest.java
+++ src/main/java/acikgoz/kaan/UserSecurityAPI/dto/request/LoginRequest.java
@@ -1,2 +1,22 @@
-package acikgoz.kaan.UserSecurityAPI.dto.request;public class LoginRequest {
+package acikgoz.kaan.UserSecurityAPI.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    @Email(message = "provide a valid email")
+    private String email;
+
+    @NotBlank(message = "provide a password")
+    private String password;
+
 }
--- src/main/java/acikgoz/kaan/UserSecurityAPI/dto/request/RegisterRequest.java
+++ src/main/java/acikgoz/kaan/UserSecurityAPI/dto/request/RegisterRequest.java
@@ -1,2 +1,28 @@
-package acikgoz.kaan.UserSecurityAPI.dto.request;public class RegisterRequest {
+package acikgoz.kaan.UserSecurityAPI.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email(message = "provide valid e-mail")
+    private String email;
+
+    @NotBlank
+    private String password;
+
 }
