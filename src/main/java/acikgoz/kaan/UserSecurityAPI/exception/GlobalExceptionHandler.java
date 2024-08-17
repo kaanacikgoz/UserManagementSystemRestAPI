328bda78143b4bd6e48d9ff1012494238158e72a
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-08-17 04:19:19 +0300

    Edited

--- src/main/java/acikgoz/kaan/UserSecurityAPI/exception/GlobalExceptionHandler.java
+++ src/main/java/acikgoz/kaan/UserSecurityAPI/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package acikgoz.kaan.UserSecurityAPI.exception;
 
-import acikgoz.kaan.UserSecurityAPI.message.ApiResponseError;
+import acikgoz.kaan.UserSecurityAPI.dto.response.ApiErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,7 +12,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<Object> handleConflictException(ConflictException e,
                                                           WebRequest request) {
-        ApiResponseError error = new ApiResponseError(
+        ApiErrorResponse error = new ApiErrorResponse(
                 HttpStatus.NOT_FOUND, e.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<>(error, error.getStatus());
