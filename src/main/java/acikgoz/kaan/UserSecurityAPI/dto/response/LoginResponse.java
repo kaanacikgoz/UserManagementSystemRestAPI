788d64742f2994b76073512ffb0c6a4b3dd09206
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-08-17 04:10:05 +0300

    Added dto responses for register and login

--- src/main/java/acikgoz/kaan/UserSecurityAPI/dto/response/LoginResponse.java
+++ src/main/java/acikgoz/kaan/UserSecurityAPI/dto/response/LoginResponse.java
@@ -1,2 +1,16 @@
-package acikgoz.kaan.UserSecurityAPI.dto.response;public class LoginResponse {
+package acikgoz.kaan.UserSecurityAPI.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String token;
+
 }
--- src/main/java/acikgoz/kaan/UserSecurityAPI/dto/response/RegisterResponse.java
+++ src/main/java/acikgoz/kaan/UserSecurityAPI/dto/response/RegisterResponse.java
@@ -1,2 +1,17 @@
-package acikgoz.kaan.UserSecurityAPI.dto.response;public class RegisterResponse {
+package acikgoz.kaan.UserSecurityAPI.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterResponse {
+
+    private String message;
+    private Boolean status;
+
 }
