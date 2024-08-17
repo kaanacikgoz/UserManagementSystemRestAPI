a5ab1a63214e914a381be7319a9b5e561b10e728
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-08-17 03:35:03 +0300

    Created role entity and added this relation to user entity

--- src/main/java/acikgoz/kaan/UserSecurityAPI/entity/Role.java
+++ src/main/java/acikgoz/kaan/UserSecurityAPI/entity/Role.java
@@ -1,2 +1,36 @@
-package acikgoz.kaan.UserSecurityAPI.entity;public class Role {
+package acikgoz.kaan.UserSecurityAPI.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType type;
+
+    @Getter
+    public enum RoleType {
+        EMPLOYEE("Employee"),
+        ADMIN("Admin");
+
+        private final String name;
+
+        RoleType(String name) {
+            this.name = name;
+        }
+
+    }
+
 }
--- src/main/java/acikgoz/kaan/UserSecurityAPI/entity/User.java
+++ src/main/java/acikgoz/kaan/UserSecurityAPI/entity/User.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
-@Table
+@Table(name = "users")
 public class User {
 
     @Id
@@ -27,7 +29,21 @@ public class User {
     @Column(length = 80, unique = true, nullable = false)
     private String email;
 
-    @Column(length = 50, nullable = false)
+    @Column(length = 100, nullable = false)
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User(String firstName, String lastName, String email, String password, Set<Role> roles) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
 
 }
