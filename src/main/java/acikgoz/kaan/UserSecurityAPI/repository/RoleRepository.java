19c0d4ef4d8736d4ebbd34ed8c7a9ae1031459cb
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-08-17 03:42:14 +0300

    Added derived query methods to both repositories

--- src/main/java/acikgoz/kaan/UserSecurityAPI/repository/RoleRepository.java
+++ src/main/java/acikgoz/kaan/UserSecurityAPI/repository/RoleRepository.java
@@ -1,2 +1,13 @@
-package acikgoz.kaan.UserSecurityAPI.repository;public class RoleRepository {
+package acikgoz.kaan.UserSecurityAPI.repository;
+
+import acikgoz.kaan.UserSecurityAPI.entity.Role;
+import acikgoz.kaan.UserSecurityAPI.entity.Role.RoleType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByType(RoleType roleType);
+
 }
