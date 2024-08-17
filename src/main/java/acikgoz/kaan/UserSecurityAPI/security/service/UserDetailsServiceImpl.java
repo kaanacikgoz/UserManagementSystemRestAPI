ff309288d1327c5c5c5d1278e795e225429b27ce
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-08-17 04:03:26 +0300

    Done userDetails and userDetailsService for security filter

--- src/main/java/acikgoz/kaan/UserSecurityAPI/security/service/UserDetailsServiceImpl.java
+++ src/main/java/acikgoz/kaan/UserSecurityAPI/security/service/UserDetailsServiceImpl.java
@@ -1,2 +1,26 @@
-package acikgoz.kaan.UserSecurityAPI.security.service;public class UserDetailsServiceImpl {
+package acikgoz.kaan.UserSecurityAPI.security.service;
+
+import acikgoz.kaan.UserSecurityAPI.entity.User;
+import acikgoz.kaan.UserSecurityAPI.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.getUserByEmail(email);
+
+        return UserDetailsImpl.build(user);
+    }
+
 }
