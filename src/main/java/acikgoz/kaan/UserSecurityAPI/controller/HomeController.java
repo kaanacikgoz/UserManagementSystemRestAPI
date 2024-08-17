ae91951d9e92da0448f7563da066b570fe821887
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-08-17 04:06:37 +0300

    Added homepage endpoints after login for use token

--- src/main/java/acikgoz/kaan/UserSecurityAPI/controller/HomeController.java
+++ src/main/java/acikgoz/kaan/UserSecurityAPI/controller/HomeController.java
@@ -1,2 +1,16 @@
-package acikgoz.kaan.UserSecurityAPI.controller;public class HomeController {
+package acikgoz.kaan.UserSecurityAPI.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/homepage")
+    public String homepage(@AuthenticationPrincipal UserDetails userDetails) {
+        return "Welcome to the homepage, " +userDetails.getUsername()+ "!";
+    }
+
 }
